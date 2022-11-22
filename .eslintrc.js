f848c73f057459746c9405d42634539b90573b7c
soonwoo <sw5063@naver.com>
2022-11-22 20:56:57 +0900

    기본 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        printWidth: 100,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        quoteProps: 'as-needed',
+        tabWidth: 2,
+        useTabs: false,
+        trailingComma: 'none'
+      }
+    ]
+  }
+};
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Vue Todo with Typescript테스트</h1>
+    <TodoInput></TodoInput>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TodoInput from './components/TodoInput.vue';
+
+export default Vue.extend({ components: { TodoInput } });
+</script>
+
+<style scoped></style>
